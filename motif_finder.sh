81a88e6570f91b6c4ec9ad001af37910647e8afe
userrp <109092365+rpecuch@users.noreply.github.com>
2024-07-15 17:52:08 -0400

    added while loop to loop through lines of file

--- motif_finder.sh
+++ motif_finder.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+# Loop through lines of motif list file
+while read line
+do
+    echo Number of occurences of $line: number
+done < motif_list.txt # Motif list file is the input rather than input from user
