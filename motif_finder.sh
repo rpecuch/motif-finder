1180cc0a1a7dd94d3c543eb848d5df2847644ac0
userrp <109092365+rpecuch@users.noreply.github.com>
2024-07-16 13:09:44 -0400

    added code to find number of occurrences of motif and write to a file

--- motif_finder.sh
+++ motif_finder.sh
@@ -3,5 +3,12 @@
 # Loop through lines of motif list file
 while read line
 do
-    echo Number of occurences of $line: number
+    # Search for number of occurrences of motif in genome file by printing out each match on a new line then counting the number of lines
+    number=`grep -o $line sample_genome.txt | wc -l`
+    # Print out results
+    echo Number of occurences of $line: $number
+    # Add results to motif_count.txt
+    #echo Number of occurences of $line: $number >> motif_count.txt
+    # Write each gene with a match to FASTA file
+    fileName=$line.txt
 done < motif_list.txt # Motif list file is the input rather than input from user
