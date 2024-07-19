7ebd8629bf8b334f320804c14587e493ad0d22ee
userrp <109092365+rpecuch@users.noreply.github.com>
2024-07-19 16:58:12 -0400

    allowed script to accept command line args

--- motif_finder.sh
+++ motif_finder.sh
@@ -4,14 +4,14 @@
 mkdir -p motifs/
 
 # Define variables available to script
-motifs_to_search=`cat interesting_motifs.txt`
-input_genome=`cat r_bifella.fasta`
+motifs_to_search=`cat $1`
+input_genome=$2
 
 # Loop through lines of motif list file
 for line in $motifs_to_search
 do
     # Search for number of occurrences of motif in FASTA file by printing out each match on a new line then counting the number of lines
-    number=`echo $input_genome | grep -o $line | wc -l`
+    number=`grep -o $line ./$input_genome | wc -l`
 
     # Print out results
     echo "Number of occurences of $line: $number"
@@ -19,10 +19,10 @@ do
     # Add results to motif_count.txt
     echo $line $number >> motif_count.txt
 
-    # Define output file name
+    # # Define output file name
     fileName=motifs/$line.txt
 
     # Write out all matching sequences to output file, include gene name by printing out the line before each sequence with match(es)
-    grep -B1 $line r_bifella.fasta >> $fileName
+    grep -B1 $line ./$input_genome >> $fileName
 
 done 
